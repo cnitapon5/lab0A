0f3c9fbc22101dbe5f622b5d8d69dd17eeb06ed1
cnitapon5 <64647643+cnitapon5@users.noreply.github.com>
2022-11-28 16:33:21 +0700

    WITCHAPON

---
+++ lab0A/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ชื่อ-สกุล นายวิชญ์พล ยืนยง");
+Console.WriteLine("รหัสนักศึกษา 653450103-3");
+Console.WriteLine("อีเมล witchaphon.y@kkumail.com");
